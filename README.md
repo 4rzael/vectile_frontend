9e538a263224d79712ab548d1a8dee16d43b0f8a
4rzael <maxime.agor23@gmail.com>
2016-10-06 18:25:23 +0200

    Added CLI documentation

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # vectile_frontend
 A really basic leaflet app to render vector tiles served by TileStache, for example
 
+## CLI
+
+* -o : Output port. Default `3000`
+
+* -h : Tile host. Default `localhost`
+
+* -p : Tile port. Default `8080`
+
+* -l : Tile layer. Default `GIS`
+
+* --help : help
+
 ## Screenshots :
 
 ![leaflet](https://cloud.githubusercontent.com/assets/9220115/19155610/64850bbc-8bdf-11e6-8700-88bdd7bdfa43.png)
--- index.js
+++ index.js
@@ -5,34 +5,42 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-
-// get index.html
-var publicFolder = path.join(__dirname, './public/');
-var index = require('fs').readFileSync(path.join(publicFolder, './index.html'));
-
 // Handle CLI
 var argv = require('minimist')(process.argv.slice(2));
 
-var port = argv.o || 3000;
-var tileHost = argv.h || 'http://localhost';
-var tilePort = argv.p || '8080';
-var tileLayer = argv.l || 'GIS'
+if (argv['help']) {
+	console.log('HELP :')
+	console.log('-o : Output port. Default "3000"');
+	console.log('-h : Tile host. Default "localhost"');
+	console.log('-p : Tile port. Default "8080"');
+	console.log('-l : Tile layer. Default "GIS"');
+} else {
+
+	// get index.html
+	var publicFolder = path.join(__dirname, './public/');
+	var index = require('fs').readFileSync(path.join(publicFolder, './index.html'));
+
+	var port = argv.o || 3000;
+	var tileHost = argv.h || 'http://localhost';
+	var tilePort = argv.p || '8080';
+	var tileLayer = argv.l || 'GIS'
 
 
-// Handle file serving
+	// Handle file serving
 
-index = index.toString()
-	.replace('TILE_HOST', tileHost)
-	.replace('TILE_PORT', tilePort)
-	.replace('TILE_LAYER', tileLayer);
+	index = index.toString()
+		.replace('TILE_HOST', tileHost)
+		.replace('TILE_PORT', tilePort)
+		.replace('TILE_LAYER', tileLayer);
 
-app.get('/', function (req, res) {
-	res.send(index);
-});
+	app.get('/', function (req, res) {
+		res.send(index);
+	});
 
 
-// Handle http server
+	// Handle http server
 
-server.listen(port);
-console.log('listening on port', port);
-console.log('Looking for tiles at', tileHost+':'+tilePort+'/'+tileLayer+'/{X}/{Y}/{Z}.json');
+	server.listen(port);
+	console.log('listening on port', port);
+	console.log('Looking for tiles at', tileHost+':'+tilePort+'/'+tileLayer+'/{X}/{Y}/{Z}.json');
+}
